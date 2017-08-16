2fb90295a193711ceedd48b8d67757b9e55ae2f9
Justin Hugon <jhugon@fnal.fake>
2017-08-16 12:55:06 -0500

    describe selection, Siunitx, don't put fonts in pdf

--- make.sh
+++ make.sh
@@ -14,6 +14,9 @@ echo "Second run of pdflatex..."
 pdflatex -interaction=batchmode --output-directory=build main
 echo "Third run of lulatex..."
 pdflatex -interaction=batchmode --output-directory=build main
-echo "Running gs to embed fonts..."
-gs -dNOPAUSE -dBATCH -dEmbedAllFonts=true -dPDFSETTINGS=/prepress -sDEVICE=pdfwrite -o build/main_fontsEmbeded.pdf build/main.pdf >& build/logGS
-cp build/main_fontsEmbeded.pdf LArIAT_PionAbsChEx_TechNote.pdf
+
+#echo "Running gs to embed fonts..."
+#gs -dNOPAUSE -dBATCH -dEmbedAllFonts=true -dPDFSETTINGS=/prepress -sDEVICE=pdfwrite -o build/main_fontsEmbeded.pdf build/main.pdf >& build/logGS
+#cp build/main_fontsEmbeded.pdf LArIAT_PionAbsChEx_TechNote.pdf
+
+cp build/main.pdf LArIAT_PionAbsChEx_TechNote.pdf
